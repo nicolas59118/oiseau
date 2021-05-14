3193afa1d0222e006babd0ad1342b0991b546acb
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-05-14 23:41:34 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ EOF
 echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
-
+install_3proxy
 
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
