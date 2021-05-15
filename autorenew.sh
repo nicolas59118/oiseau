d865be3c9939ebe7ae023c69499435521785d9bc
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-05-15 20:37:58 +0200

    Add files via upload

--- autorenew.sh
+++ autorenew.sh
@@ -11,7 +11,19 @@ gen64() {
   }
   echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
+install_3proxy() {
+  echo "installing 3proxy"
+  URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+  wget -qO- $URL | bsdtar -xvf-
+  cd 3proxy-3proxy-0.8.6
+  make -f Makefile.Linux
+  mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+  cp src/3proxy /usr/local/etc/3proxy/bin/
+  cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+  chmod +x /etc/init.d/3proxy
+  chkconfig 3proxy on
+  cd $WORKDIR
+}
 
 gen_3proxy() {
   cat <<EOF
@@ -31,8 +43,28 @@ $(awk -F "/" '{print "auth none\n" \
 EOF
 }
 
+gen_proxy_file_for_user() {
+  cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
+}
 
+upload_proxy() {
+  local PASS=$(random)
+  zip --password $PASS proxy.zip proxy.txt
+  URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
 
+  echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+  echo "Download zip archive from: ${URL}"
+  echo "Password: ${PASS}"
+
+}
+
+install_jq() {
+  wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+  chmod +x ./jq
+  cp jq /usr/bin
+}
 
 upload_2file() {
   local PASS=$(random)
@@ -46,7 +78,7 @@ upload_2file() {
 
 gen_data() {
   seq $FIRST_PORT $LAST_PORT | while read port; do
-    echo "//$IP4/$port/$(gen64 $IP6)"
+    echo "usr/pass/$IP4/$port/$(gen64 $IP6)"
   done
 }
 
@@ -61,14 +93,12 @@ gen_ifconfig() {
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
 
+install_3proxy
 
-
-
-pkill 3proxy
-
-
-
+echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
@@ -76,12 +106,12 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "First port used for proxy? Example 10000 "
-read FIRST_PORT
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+
 echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-
+FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
@@ -94,12 +124,15 @@ gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 20096
-service 3proxy restart
+ulimit -n 19048
+service 3proxy start
 EOF
 
 bash /etc/rc.local
 
+gen_proxy_file_for_user
 
+# upload_proxy
 
+install_jq && upload_2file
 
