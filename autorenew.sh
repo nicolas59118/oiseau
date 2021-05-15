58229028dd072acb91b8bb5b947ce86aecf751bc
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-05-15 20:35:51 +0200

    Add files via upload

--- autorenew.sh
+++ autorenew.sh
@@ -76,11 +76,12 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
+echo "First port used for proxy? Example 10000 "
+read FIRST_PORT
+echo "How many proxy do you want to create? Example 500"
+read COUNT
 
 
-
-FIRST_PORT=10000
-COUNT=32
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
@@ -89,9 +90,6 @@ gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-rm -rf /home/proxy-installer/boot_iptables.sh
-rm -rf /home/proxy-installer/boot_ifconfig.sh
 
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
