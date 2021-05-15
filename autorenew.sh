37d0619febd022886e7d2c145ba29b5a163a0900
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-05-15 21:02:02 +0200

    Add files via upload

--- autorenew.sh
+++ autorenew.sh
@@ -122,11 +122,14 @@ echo "-----------------------------"
 echo "-----------------------------"
 
 
+echo "Quel Port ? 10000"
+read $FIRST_PORT
 
 echo "How many proxy do you want to create? Example 500"
 read COUNT
 
-FIRST_PORT=10000
+
+
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 
