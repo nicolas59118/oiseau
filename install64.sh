f3203d6c136e4d58c9d18cd42d7d199c5167533b
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-06-18 12:51:13 +0200

    Update install64.sh

--- install64.sh
+++ install64.sh
@@ -9,7 +9,7 @@ gen64() {
   ip64() {
     echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
   }
-  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64)"
+  echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
   echo "installing 3proxy"
@@ -104,7 +104,7 @@ WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
-IP6=2a07:8dc0:19:13b:
+IP6=2a07:8dc0:19:13b
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
