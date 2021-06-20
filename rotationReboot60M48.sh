045c808ffb9705a6bc8438f039cefec8911e4aec
nicolas59118 <70537564+nicolas59118@users.noreply.github.com>
2021-06-21 00:30:59 +0200

    Update rotationReboot60M48.sh

--- rotationReboot60M48.sh
+++ rotationReboot60M48.sh
@@ -102,7 +102,7 @@ WORKDATA="${WORKDIR}/data.txt"
 cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
-IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+IP6=$(curl -6 -s icanhazip.com | cut -f1-3 -d':')
 
 COUNT=264
 
